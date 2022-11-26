29a3dd325220ae1a970118f02f293b08354bfabd
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-27 00:19:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+class Polygon {
+  constructor(nums) {
+    this.nums = nums;
+  }
+  get countSides() {
+    return this.nums.length;
+  }
+  get perimeter() {
+    return this.nums.reduce((acc, curr) => acc + curr);
+  }
+}
+class Triangle extends Polygon {
+  get isValid() {
+    let is = false;
+    for (let i = 1; i < this.nums.length; i++) {
+      if (this.nums[i - 1] == this.nums[i]) {
+        is = true;
+      } else {
+        is = false;
+      }
+    }
+    return is;
+  }
+}
+class Square extends Polygon {
+  get isValid() {
+    let is = false;
+    for (let i = 1; i < this.nums.length; i++) {
+      if (this.nums[i - 1] == this.nums[i]) {
+        is = true;
+      } else {
+        is = false;
+      }
+    }
+    return is;
+  }
+  get area() {
+    return this.nums[0] ** 2;
+  }
+}
